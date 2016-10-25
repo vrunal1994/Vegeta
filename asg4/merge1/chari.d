8525353b42093f60a02bb67d4bdd2e244cd0a3cb
sravira <santhoshrajravirala007@gmail.com>
2016-10-25 10:28:45 -0400

    merge1

---
+++ asg4/merge1/chari.d
@@ -0,0 +1,2 @@
+chari.o chari.d : chari.cpp chari.h drawable.h vector2f.h frame.h sprite.h \
+ gamedata.h parseXML.h ioManager.h frameFactory.h
---
+++ asg4/merge1/hud.d
@@ -0,0 +1,1 @@
+hud.o hud.d : hud.cpp hud.h aaline.h gamedata.h vector2f.h parseXML.h
